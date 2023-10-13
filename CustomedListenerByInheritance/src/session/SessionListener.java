ab9483438918d01884104baf00c232c8a6653980
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-10-13 08:58:40 +0300

    valueBound

--- CustomedListenerByInheritance/src/session/SessionListener.java
+++ CustomedListenerByInheritance/src/session/SessionListener.java
@@ -8,26 +8,26 @@ import jakarta.servlet.http.HttpSessionBindingListener;
 
 public class SessionListener implements HttpSessionBindingListener{
 
-    
-    public SessionModel getModel(HttpSessionBindingEvent event)
-    {
-        PosgtreConnection pg = ConnectionHolder.getConnection();
-        SessionModel sModel= new SessionModel(pg);
-        sModel.setCryptedIDSession(SessionHolder.getSessionValue());
-        sModel.setKey(event.getName());
-        sModel.setValue(event.getValue().toString());
-        return sModel;
-    }
+
 
     @Override
-    public void valueBound(HttpSessionBindingEvent event) {
+    public void valueBound(HttpSessionBindingEvent event) 
+    {
         /*
          * Add session to database
          */
-
-        
-
-
+        SessionModel sModel = getModel(event);
+        try 
+        {
+            if  (!sModel.updateSessionData()) 
+            {   
+                sModel.create();
+            }
+        }
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
     
     }
 
@@ -36,6 +36,17 @@ public class SessionListener implements HttpSessionBindingListener{
         /*
          * Remove session from database
          */
+    }
+
+        
+    public SessionModel getModel(HttpSessionBindingEvent event)
+    {
+        PosgtreConnection pg = ConnectionHolder.getConnection();
+        SessionModel sModel= new SessionModel(pg);
+        sModel.setCryptedIDSession(SessionHolder.getSessionValue());
+        sModel.setKey(event.getName());
+        sModel.setValue(event.getValue().toString());
+        return sModel;
     }
     
 }

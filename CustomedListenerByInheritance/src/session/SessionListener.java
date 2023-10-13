cf327b2abe350c2ea1968133c7f21a91f7ff6ec6
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-10-13 08:55:31 +0300

    getModel

--- CustomedListenerByInheritance/src/session/SessionListener.java
+++ CustomedListenerByInheritance/src/session/SessionListener.java
@@ -1,17 +1,34 @@
 package session;
 
+import databaseAccess.ConnectionHolder;
+import databaseAccess.PosgtreConnection;
+import databaseAccess.SessionModel;
 import jakarta.servlet.http.HttpSessionBindingEvent;
 import jakarta.servlet.http.HttpSessionBindingListener;
 
 public class SessionListener implements HttpSessionBindingListener{
 
     
+    public SessionModel getModel(HttpSessionBindingEvent event)
+    {
+        PosgtreConnection pg = ConnectionHolder.getConnection();
+        SessionModel sModel= new SessionModel(pg);
+        sModel.setCryptedIDSession(SessionHolder.getSessionValue());
+        sModel.setKey(event.getName());
+        sModel.setValue(event.getValue().toString());
+        return sModel;
+    }
 
     @Override
     public void valueBound(HttpSessionBindingEvent event) {
         /*
          * Add session to database
          */
+
+        
+
+
+    
     }
 
     @Override
